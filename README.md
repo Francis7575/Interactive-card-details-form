17c7717253f9b9c30e9205d06007ed342925d5b1
Francis7575 <francis.albert.2009@gmail.com>
2024-03-27 22:12:01 -0700

    first commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# Interactive-card-details-form
---
+++ react-interactive-card-form/src/components/MainContent.tsx
@@ -0,0 +1,11 @@
+import styles from './MainContent.module.css'
+
+const MainContent = () => {
+    return (
+        <main>
+            <aside className={styles.aside}></aside>
+        </main>
+    )
+}
+
+export default MainContent
